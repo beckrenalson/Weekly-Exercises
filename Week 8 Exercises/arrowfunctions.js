e4b99ad9ff3c5716214004c2da1b29ce40f8393c
beckrenalson <beck.take2@gmail.com>
2025-03-11 17:04:40 +1300

    arrow functions, string arrays

--- Week 8 Exercises/arrowfunctions.js
+++ Week 8 Exercises/arrowfunctions.js
@@ -1,1 +1,4 @@
+let bobprinter = (name) => name == "Bob";
 
+console.log(bobprinter("Bob"));
+console.log(bobprinter("notBob"));
---
+++ Week 8 Exercises/stringarray.js
@@ -0,0 +1,8 @@
+let indexing = ['w', 'o', 'r', 'l', 'd'];
+
+indexing[2] = "R";
+console.log(indexing);
+
+let convString = ['h ', 'e ', 'l ', 'l ', 'o']
+
+console.log(convString);
