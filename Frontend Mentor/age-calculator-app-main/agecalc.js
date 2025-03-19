e8c49fb43cbbafbc4560940f0a76ad0876bba295
beckrenalson <beck.take2@gmail.com>
2025-03-19 16:36:05 +1300

    milliseconds to year, month, day output

--- Frontend Mentor/age-calculator-app-main/agecalc.js
+++ Frontend Mentor/age-calculator-app-main/agecalc.js
@@ -9,11 +9,10 @@ function getBirthday() {
 
     if (!day || !month || !year) {
         alert("Please enter a day, month, and year.");
-        return getBirthday;
     } else {
-        let birthday = `${year}-${month}-${day}`;
-        console.log(birthday);
-        return getBirthday;
+        let birthdayOutput = `${year}-${month}-${day}`;
+        console.log(birthdayOutput);
+        return birthdayOutput;
     }
 }
 
@@ -23,16 +22,41 @@ function currentDate() {
     const currentMonth = date.getMonth() + 1;
     const currentDay = date.getDate();
 
-    console.log(`${currentYear}-${currentMonth}-${currentDay}`);
-} 
+    let dateOutput = `${currentYear}-${currentMonth}-${currentDay}`;
+    console.log(dateOutput);
+    return dateOutput;
+}
 
+function ageOutput() {
+    let date1 = new Date(getBirthday());
+    let date2 = new Date(currentDate());
+    let dateDifference = date2 - date1;
+    console.log(dateDifference);
 
-// const currentDay = new Date.prototype.getDate();
-// console.log(currentDay);
+    let numberOfMillisecondsInOneYear = 365 * 24 * 60 * 60 * 1000;
+    let numberOfYears = dateDifference / numberOfMillisecondsInOneYear;
+    console.log(numberOfYears);
 
+    let remainderMilliseconds = dateDifference % numberOfMillisecondsInOneYear;
+    console.log(remainderMilliseconds);
 
+    let numberOfMillisecondsInOneMonth = 30 * 24 * 60 * 60 * 1000;
+    let numberOfMonths = remainderMilliseconds / numberOfMillisecondsInOneMonth;
+    console.log(numberOfMonths);
 
-// let dayDifference = parseInt(day) - currentDate;
+    remainderMilliseconds = dateDifference % numberOfMillisecondsInOneMonth;
+    console.log(remainderMilliseconds);
 
-submitData.addEventListener("click", getBirthday);
+    let numberOfMillisecondsInOneDay = 24 * 60 * 60 * 1000;
+    let numberOfDays = remainderMilliseconds / numberOfMillisecondsInOneDay;
+    console.log(numberOfDays);
+    
+    // let dayDifference = currentDate() - getBirthday();
+    // console.log(parseInt(dayDifference));
+    // let outputYear = document.getElementById("yearOutput").innerText;
+    // let outputMonth = document.getElementById("monthOutput").innerText;
+    // let outputDay = document.getElementById("dayOutput").innerText;
+}
+
+submitData.addEventListener("click", ageOutput);
 currentDate();
