0ff6061826a80d7b474f29b2fec9ea407e178887
beckrenalson <beck.take2@gmail.com>
2025-03-25 16:47:08 +1300

    js

--- Frontend Mentor/mortgage-repayment-calculator-main/mortgagecalc.js
+++ Frontend Mentor/mortgage-repayment-calculator-main/mortgagecalc.js
@@ -1,1 +1,3 @@
-let amount = document.querySelector(".mortgageAmount");
+let amount = document.getElementById("mortgageAmount").value;
+let term = document.getElementById("mortgageTerm").value;
+let rate = document.getElementById("mortgageRate").value;
